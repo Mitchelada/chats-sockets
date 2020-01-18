6696c51ec901ea66f9ef68b4d5ba8a05717fa8eb
MichelAraya <michel.araya@live.com>
2020-01-18 02:53:31 -0300

    version 0.5

---
+++ public/js/socket-chat.js
@@ -0,0 +1,60 @@
+var socket = io();
+
+
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html'
+    throw new Error('El nombre y sala son necesario')
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
+    })
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+
+
+// Escuchar información
+socket.on('crearMensaje', (mensaje) => {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+//Escuchar cambios de usuarios
+// Cuando un usuario entra o sale del chat
+
+socket.on('listarPersona', (personas) => {
+    console.log(personas);
+})
+
+//Mensajes privados
+socket.on('mensajePrivado', (mensaje) => {
+
+    console.log('Mensaje privado: ', mensaje);
+})
